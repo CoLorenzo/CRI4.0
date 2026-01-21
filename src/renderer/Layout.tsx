f0d8573a860275316bf58c016ff961acca41a0b1
Lorenzo Berardi <lorenzo2berardi@gmail.com>
2026-01-21 17:10:39 +0100

    removed footbar

--- src/renderer/Layout.tsx
+++ src/renderer/Layout.tsx
@@ -4,14 +4,14 @@
 
 
 import { useState, useEffect } from 'react';
-import { useNavigate, Routes, Route, Outlet} from 'react-router-dom';
+import { useNavigate, Routes, Route, Outlet } from 'react-router-dom';
 import "tailwindcss/tailwind.css";
 import Home from "./pages/Home";
 import Topology from "./pages/Topology";
 import Attack from "./pages/Attack";
 import Logs from "./pages/Logs";
-import { Footer } from './components/Footer';
-import {NextUIProvider} from '@nextui-org/react';
+
+import { NextUIProvider } from '@nextui-org/react';
 import { AppNavbar } from './components/Navbar/AppNavbar';
 import { NotificationProvider } from './contexts/NotificationContext';
 import Settings from './pages/Settings';
@@ -31,30 +31,30 @@ export default function App() {
 
   return (
     <NextUIProvider navigate={navigate}>
-       <main className={`${darkMode ? 'dark' : 'light'} text-foreground bg-background w-full flex min-h-screen`}>
-          <div className="h-full flex-1">
-            <div className="flex h-full min-h-screen flex-col justify-between">
-              <NotificationProvider>
-                <LogProvider>
-                  <AppNavbar darkMode={darkMode} setDarkMode={setDarkMode} />
-                  <div className="min-h-[calc(100vh-4rem)]">
-                    <Routes>
-                      <Route path="/" element={<Outlet />}>
-                        <Route index element={<Home />} />
-                        <Route path="topology" element={<Topology />} />
-                        <Route path="attack" element={<Attack />} />
-                        <Route path="report" element={<Report />} />
-                        <Route path="logs" element={<Logs />} />
-                        <Route path="settings" element={<Settings />} />
-                      </Route>
-                    </Routes>
-                  </div>
-                  <Footer />
-                </LogProvider>
-              </NotificationProvider>
-            </div>
+      <main className={`${darkMode ? 'dark' : 'light'} text-foreground bg-background w-full flex min-h-screen`}>
+        <div className="h-full flex-1">
+          <div className="flex h-full min-h-screen flex-col justify-between">
+            <NotificationProvider>
+              <LogProvider>
+                <AppNavbar darkMode={darkMode} setDarkMode={setDarkMode} />
+                <div className="min-h-[calc(100vh-4rem)]">
+                  <Routes>
+                    <Route path="/" element={<Outlet />}>
+                      <Route index element={<Home />} />
+                      <Route path="topology" element={<Topology />} />
+                      <Route path="attack" element={<Attack />} />
+                      <Route path="report" element={<Report />} />
+                      <Route path="logs" element={<Logs />} />
+                      <Route path="settings" element={<Settings />} />
+                    </Route>
+                  </Routes>
+                </div>
+
+              </LogProvider>
+            </NotificationProvider>
           </div>
-        </main>
+        </div>
+      </main>
     </NextUIProvider>
   );
 }
