994af1ef110b240e7563d59455e7e6d698feb015
Lorenzo Berardi <lorenzo2berardi@gmail.com>
2026-01-26 09:16:36 +0100

    improved temperature sensor

--- containers/temperature_sensor/start.sh
+++ containers/temperature_sensor/start.sh
@@ -4,4 +4,21 @@
 ENDPOINT="${ENDPOINT:-http://localhost:8000/}"
 
 # Run the temperature sensor with the endpoint
-exec uv run /tsens.py -e "$ENDPOINT"
+# Build the command arguments
+ARGS="-e $ENDPOINT"
+
+if [ "${SINE_WAVE:-false}" = "true" ]; then
+    ARGS="$ARGS --sine"
+    if [ -n "$SINE_PERIOD" ]; then
+        ARGS="$ARGS --period $SINE_PERIOD"
+    fi
+    if [ -n "$SINE_AMPLITUDE" ]; then
+        ARGS="$ARGS --amplitude $SINE_AMPLITUDE"
+    fi
+    if [ -n "$SINE_OFFSET" ]; then
+        ARGS="$ARGS --offset $SINE_OFFSET"
+    fi
+fi
+
+# Run the temperature sensor with the constructed arguments
+exec uv run /tsens.py $ARGS
